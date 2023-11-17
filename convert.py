7cb6a800c7415033ffb476a14d13999f145f8bcc
Vortex <73261680+Vortex2Oblivion@users.noreply.github.com>
2023-11-17 22:11:31 +0000

    Update convert.py

--- convert.py
+++ convert.py
@@ -3,10 +3,7 @@ import os
 
 print("converting shader frag...")
 
-replacements = {'void mainImage( out vec4 fragColor, in vec2 fragCoord )':'void main()', 'void mainImage(out vec4 fragColor, in vec2 fragCoord)':'void main()' 'texture':'flixel_texture2D','iChannel0':'bitmap',
-                'fragCoord / iResolution.xy':'openfl_TextureCoordv', 'fragCoord.xy / iResolution.xy':'openfl_TextureCoordv', 'fragCoord.xy/iResolution.xy':'openfl_TextureCoordv', 
-                'fragCoord/iResolution.xy':'openfl_TextureCoordv', 'fragCoord/iResolution':'openfl_TextureCoordv.xy', 'fragColor':'gl_FragColor'
-               }
+replacements = {'void mainImage( out vec4 fragColor, in vec2 fragCoord )':'void main()', 'void mainImage(out vec4 fragColor, in vec2 fragCoord)':'void main()', 'texture':'flixel_texture2D','iChannel0':'bitmap','fragCoord / iResolution.xy':'openfl_TextureCoordv', 'fragCoord.xy / iResolution.xy':'openfl_TextureCoordv', 'fragCoord.xy/iResolution.xy':'openfl_TextureCoordv', 'fragCoord/iResolution.xy':'openfl_TextureCoordv', 'fragCoord/iResolution':'openfl_TextureCoordv.xy', 'fragColor':'gl_FragColor'}
 
 
 
